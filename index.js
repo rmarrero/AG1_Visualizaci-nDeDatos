e298c732ce76e8cc089831257290ffcc400e2a5c
rmarrero <ricardo.marrerocarrion@gmail.com>
2022-04-27 12:24:18 +0200

    AG3 V0.1

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 // Import stylesheets
 import './style.css';
 
+//Gráfico de columnas. Mi 1o gráfico
+var visualization = d3plus.viz()
+    .container("#viz1")
+    .data([{"year": 1991, "name":"alpha", "value": 15},
+          {"year": 1992, "name":"alpha", "value": 20},])
+    .type("bar")
+    .id("name")
+    .x("year")
+    .y("value")
+    .draw()
+
+// Gráfico de Multicolumnas
+
+    d3.json
+    ("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+      var visualization = d3plus.viz()
+      .container("#viz2")
+      .data(data)
+      .type('bar')
+      .id('name')
+      .x('year')
+      .y('value')
+      .axes({ ticks: 'false' })
+      .draw();
+      });
+
+// Gráfico de BoxPlot
+
